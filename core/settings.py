705a0842a394dc9232b04ea3b9bf18027729ad61
BINITA GHALE <ghalebinita54@gmail.com>
2025-07-16 16:03:29 +0545

    Edited the database and requirement.txt

--- core/settings.py
+++ core/settings.py
@@ -73,14 +73,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydb',
-        'USER': 'root',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Djangodb',
+        'USER': 'Djangouser',
         'PASSWORD': '',
         'HOST': 'localhost',
-        'PORT': '3306',
+        'PORT': '5432',
     }
 }
+
 
 
 
